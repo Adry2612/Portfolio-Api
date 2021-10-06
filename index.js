b8935e281fac388ba3c86ba84961011d006b4fa1
adrian_vidal <adrian.vidal@enzymeadvisinggroup.com>
2021-10-06 11:40:03 +0200

    Creación de variables de entorno

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 var mongoose = require('mongoose')
+require('dotenv').config()
 var app = require('./app')
-var port = process.env.port || 3700
+var port = process.env.PORT || 3700
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/portfolio')
+mongoose.connect(`mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.bqv0s.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`)
     .then(() => {
         app.listen(port, () => {
-            console.log('Servidor funcionando correctamente en la url: http://localhost:3700')
+            console.log(`Servidor funcionando correctamente en el puerto: ${port}`)
         })
     })
     .catch(err => console.log(err))
